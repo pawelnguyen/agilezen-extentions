00cb6b18e59e9d553e846856889e0aa2d9b0ae65
Paweł Nguyen <pawel.nguyen@gmail.com>
2014-03-03 12:13:49 +0100

    deal with zapier's deduplication

--- lib/agilezen.rb
+++ lib/agilezen.rb
@@ -15,12 +15,12 @@ class Agilezen
 
   def overflows_items
     phases_items.map do |p|
-      p if !p['limit'].nil? && phase_overflow?(p['id'], p['limit'])
+      coerce_id(p) if !p['limit'].nil? && phase_overflow?(p['id'], p['limit'])
     end.compact
   end
 
   def phases_items
-    JSON.parse(RestClient.get(phases_url))['items']
+    @phases_items ||= JSON.parse(RestClient.get(phases_url))['items']
   end
 
   def phases_url
@@ -33,11 +33,20 @@ class Agilezen
   end
 
   def phase_stories(phase_id)
-    JSON.parse(RestClient.get(stories_url(phase_id)))
+    @phase_stories ||= {}
+    @phase_stories[phase_id] ||= JSON.parse(RestClient.get(stories_url(phase_id)))
   end
 
   def stories_url(phase_id)
     "#{PROJECTS_URL}#{@project_id}/stories.json?apikey=#{@api_key}&where=phase:#{phase_id}"
+  end
+
+  def coerce_id(phase) # a little hack to deal with zapier deduplication https://zapier.com/developer/documentation/reference/#deduplication
+    phase.dup.tap { |p| p['id'] = "#{last_story_id(p['id'])}#{p['id']}".to_i }
+  end
+
+  def last_story_id(phase_id)
+    phase_stories(phase_id)['items'].last['id']
   end
 
   class << self
